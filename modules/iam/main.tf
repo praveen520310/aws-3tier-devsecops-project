85a12bef87e6a3a63b993c3ffbdbaacec26c2728
Paul Praveen Kumar Athmuri <praveen.athmuri@gmail.com>
2026-09-04 16:19:17 +0530

    Add SSM permissions to EC2 IAM role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -32,3 +32,8 @@ resource "aws_iam_instance_profile" "ec2" {
     Environment = var.environment
   }
 }
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
